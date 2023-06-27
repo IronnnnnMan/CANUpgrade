9c0ca7bd8d52426c753fc001015e57f311217a80
chenjunguang93 <chenjunguang93@163.com>
2023-06-27 09:40:41 +0800

    GUI标题更新为V3.0

--- CANUpgrade/Form1.Designer.cs
+++ CANUpgrade/Form1.Designer.cs
@@ -354,7 +354,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "CAN upgrade service";
+            this.Text = "CAN upgrade service V3.0";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.UpgradeServicePanel.ResumeLayout(false);
             this.UpgradeServicePanel.PerformLayout();
